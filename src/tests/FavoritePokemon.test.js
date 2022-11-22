25a3a934b8157865b801f3a3792ac05d229183cc
Lucas Portilho <lucasportilho18@gmail.com>
2022-11-22 16:06:47 -0300

    requisito 3

--- src/tests/FavoritePokemon.test.js
+++ src/tests/FavoritePokemon.test.js
@@ -1,1 +1,13 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import { FavoritePokemon } from '../pages';
+
+describe('testando o componente FavoritePokemon', () => {
+  test('testando se é exibida a mensagem No favorite pokemno found, caso não tenha Pokémon favoritos', () => {
+    renderWithRouter(<FavoritePokemon />);
+
+    const favoriteTitle = screen.getByText('No favorite Pokémon found');
+    expect(favoriteTitle).toBeInTheDocument();
+  });
+});
